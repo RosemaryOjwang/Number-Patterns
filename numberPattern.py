31ff7f77e77cf3b9eaf2d0a980016229f2290a85
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2022-11-21 15:18:48 +0300

    add code for another pattern

--- numberPattern.py
+++ numberPattern.py
@@ -7,3 +7,11 @@ for k in range(number+1, 1, -1):
   for l in range(1, k-1):
     print(l, end="")
   print("\r")
+for w in range(number+1, 1, -1):
+  for x in range(1, w-1):
+    print(x, end="")
+  print("\r")
+for y in range(1, number+1):
+  for z in range(1, y+1):
+    print(z, end="")
+  print("\r")
